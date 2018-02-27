c79585978c44e830dcf8ea283784f43f5cad969d
Micheal Covington <xcreepa@yahoo.com>
2018-02-26 22:58:54 -0500

    changed header

--- src/App.js
+++ src/App.js
@@ -40,13 +40,34 @@ class App extends Component {
 
   render() {
     return (
+    
       <div className="tc">
-        <header className="App-header">
-          <img src={globe} className="App-logo" alt="logo" />
-          <h1 className="App-intro code">Welcome to Travel Guide</h1>
-        </header>
+
+        <header className="bg-purple white-80 tc pv4 avenir">
+        
+  
+  <h1 className="mt2 mb3 baskerville i fw1 f1">Travel Guide</h1>
+  <img src={globe} className="App-logo" alt="logo" />
+
+  
+  <nav className="bt bb tc mw8 center mt4">
+    <a className="f6 f5-l link bg-animate white-80 hover-bg-light-purple dib pa3 ph4-l" href="#">Africa</a>
+    <a className="f6 f5-l link bg-animate white-80 hover-bg-light-purple dib pa3 ph4-l" href="#">Americas</a>
+    <a className="f6 f5-l link bg-animate white-80 hover-bg-light-purple dib pa3 ph4-l" href="#">Asia</a>
+    <a className="f6 f5-l link bg-animate white-80 hover-bg-light-purple dib pa3 ph4-l" href="#">Oceana</a>
+    <a className="f6 f5-l link bg-animate white-80 hover-bg-light-purple dib pa3 ph4-l" href="#">Polar</a>
+  </nav>
+</header>
+
+
+
+        
         <RegionList countries={this.state.countries} regions={this.state.regionList}>
         </RegionList>
+
+        
+          
+     
       </div>
     );
   }
