97764adf7268004cdcd239febc11f13e6e82aa1f
dimitrisz123 <dimitris-007@hotmail.com>
2018-07-26 21:15:22 +0300

    did a bit of cleanup

--- src/components/RegionList.js
+++ src/components/RegionList.js
@@ -18,7 +18,7 @@ const RegionList = ({ countries, regions }) => {
         />
     );
   });
-  return (<div class="centerFlex"> {regionArray} </div>);
+  return (<div className="centerFlex"> {regionArray} </div>);
 };
 
 export default RegionList;
