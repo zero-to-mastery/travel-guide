6bbe11f7a1b17a3410b5d518eebd3ad3c80af372
Ambrus Attila <attilawow91@gmail.com>
2018-10-28 12:30:12 +0200

    Convert header footer to functional components

--- src/components/Region.js
+++ src/components/Region.js
@@ -3,25 +3,25 @@ import SearchBox from '../components/SearchBox';
 import CountryCard from './CountryCard';
 import '../styles/Region.css';
 
-const Region = ({region, countries}) => {
+const Region = ({ region, countries, onSearchChange }) => {
   let countryList = countries.filter(country => country.region === region);
   let countryNames = countryList.map(country => <CountryCard key={country.name} countryData={country} />);
- 
-  return(
+
+  return (
     <div className="centerFlex">
-      <SearchBox />
+      <SearchBox onSearchChange={onSearchChange} />
       <div className="tc bg-white-90  code dib br3 pb1 ma3 bw2 shadow-5 region">
         <h2>{region}</h2>
         <ul className='list'>
           {countryNames}
         </ul>
         <a className="f6 link dim br-pill ph4 pv2 mb2 dib white bg-purple"
-        onClick={this.handleOnClick} href="history.go(-1)">BACK</a>
+          onClick={this.handleOnClick} href="history.go(-1)">BACK</a>
       </div>
-   
+
     </div>
   )
-  
+
 };
 
 export default Region;
