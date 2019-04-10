6fef48bc58b82864b9605b53637e1aaba522ac96
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-10 15:28:16 +0530

    Modifying ahref to Link from react-router-dom and adding Link to country names

--- src/components/Region.js
+++ src/components/Region.js
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBox from "../components/SearchBox";
 import CountryCard from "./CountryCard";
 import "../styles/Region.css";
+import { Link } from 'react-router-dom';
 
 const Region = ({ region, countries, search, onSearchChange, flagList }) => {
   let countryList = countries.filter(
@@ -17,13 +18,13 @@ const Region = ({ region, countries, search, onSearchChange, flagList }) => {
       <SearchBox onSearchChange={onSearchChange} search={search} />
       <div className="tc bg-white-90  code dib br3 pb1 ma3 bw2 shadow-5 region">
         <h2>{region}</h2>
-        <ul className="list">{countryNames}</ul>
-        <a
+        <Link to="/"><ul className="list">{countryNames}</ul></Link>
+        <Link
           className="f6 link dim br-pill ph4 pv2 mb2 dib white bg-purple"
-          href="/"
+          to="/"
         >
           BACK
-        </a>
+        </Link>
       </div>
     </div>
   );
