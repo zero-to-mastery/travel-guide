899d24fbf0a6b4a4eea6fc31ee6d405128725a9d
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-11 14:44:56 +0530

    redirecting Url to a new path

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,12 +12,12 @@ const header = () => {
 
       <nav className="bt bb tc center mt4 nav">
         <NavLink to='/travel-guide' activeClassName="is-active" className="f6 f5-l link bg-animate white-80 hover-bg-purple b dib pa3 ph4-l">Home</NavLink>
-        <NavLink to='/africa' activeClassName="is-active" className="f6 f5-l link bg-animate white-80 hover-bg-purple b dib pa3 ph4-l">Africa</NavLink>
-        <NavLink to='/americas' activeClassName="is-active" className="f6 f5-l link bg-animate white-80 hover-bg-purple b dib pa3 ph4-l">Americas</NavLink>
-        <NavLink to='/asia' activeClassName="is-active" className="f6 f5-l link bg-animate white-80 hover-bg-purple b dib pa3 ph4-l">Asia</NavLink>
-        <NavLink to='/europe' activeClassName="is-active" className="f6 f5-l link bg-animate white-80 hover-bg-purple b dib pa3 ph4-l">Europe</NavLink>
-        <NavLink to='/oceania' activeClassName="is-active" className="f6 f5-l link bg-animate white-80 hover-bg-purple b dib pa3 ph4-l">Oceania</NavLink>
-        <NavLink to='/polar' activeClassName="is-active" className="f6 f5-l link bg-animate white-80 hover-bg-purple b dib pa3 ph4-l">Polar</NavLink>
+        <NavLink to='/travel-guide/africa' activeClassName="is-active" className="f6 f5-l link bg-animate white-80 hover-bg-purple b dib pa3 ph4-l">Africa</NavLink>
+        <NavLink to='/travel-guide/americas' activeClassName="is-active" className="f6 f5-l link bg-animate white-80 hover-bg-purple b dib pa3 ph4-l">Americas</NavLink>
+        <NavLink to='/travel-guide/asia' activeClassName="is-active" className="f6 f5-l link bg-animate white-80 hover-bg-purple b dib pa3 ph4-l">Asia</NavLink>
+        <NavLink to='/travel-guide/europe' activeClassName="is-active" className="f6 f5-l link bg-animate white-80 hover-bg-purple b dib pa3 ph4-l">Europe</NavLink>
+        <NavLink to='/travel-guide/oceania' activeClassName="is-active" className="f6 f5-l link bg-animate white-80 hover-bg-purple b dib pa3 ph4-l">Oceania</NavLink>
+        <NavLink to='/travel-guide/polar' activeClassName="is-active" className="f6 f5-l link bg-animate white-80 hover-bg-purple b dib pa3 ph4-l">Polar</NavLink>
       </nav>
     </header>
   )
