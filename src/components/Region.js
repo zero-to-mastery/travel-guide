3e41606ccea1000e3389c0486bfb73539cc68756
luis-escoto11 <luis.escoto011@gmail.com>
2019-09-08 21:19:28 -0600

    testing first commit to the guide

--- src/components/Region.js
+++ src/components/Region.js
@@ -18,7 +18,7 @@ const Region = ({ region, countries, search, onSearchChange, flagList }) => {
   return (
     <div className="centerFlex" style={{
       backgroundImage:
-        "url(https://images.unsplash.com/photo-1498831624351-bb3e382fe091?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80)"
+        "url(https://images.unsplash.com/photo-1496950866446-3253e1470e8e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80)"
        
     }}>
       <SearchBox onSearchChange={onSearchChange} search={search} />
