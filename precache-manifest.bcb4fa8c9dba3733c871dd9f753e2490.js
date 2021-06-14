4dbddfee34aba456f450e8842163292ff142798c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-14 11:45:05 +0000

    Deploy zero-to-mastery/travel-guide to github.com/zero-to-mastery/travel-guide.git:gh-pages

---
+++ precache-manifest.bcb4fa8c9dba3733c871dd9f753e2490.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30e9f7874974e43c2a08661d00c6b97",
+    "url": "/travel-guide/index.html"
+  },
+  {
+    "revision": "217da3e692b4a204ba26",
+    "url": "/travel-guide/static/css/2.418e1221.chunk.css"
+  },
+  {
+    "revision": "ad21abab16f392cdebbd",
+    "url": "/travel-guide/static/css/main.a4fe91a8.chunk.css"
+  },
+  {
+    "revision": "217da3e692b4a204ba26",
+    "url": "/travel-guide/static/js/2.5e93255a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/travel-guide/static/js/2.5e93255a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad21abab16f392cdebbd",
+    "url": "/travel-guide/static/js/main.17fbf886.chunk.js"
+  },
+  {
+    "revision": "deccac3b3067dc853681",
+    "url": "/travel-guide/static/js/runtime-main.d994cc90.js"
+  },
+  {
+    "revision": "3b735d9eef50984339a48fa90448ef0a",
+    "url": "/travel-guide/static/media/BaganMyanmar.3b735d9e.jpg"
+  },
+  {
+    "revision": "d8c3acbd31f04801bf24e193425eaf79",
+    "url": "/travel-guide/static/media/Globe.d8c3acbd.svg"
+  }
+]);
