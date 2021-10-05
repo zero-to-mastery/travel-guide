cb79f8278fda86eeb339e7119803ff24d216a82e
Kennith Nichol <kennith.nichol@toptal.com>
2021-10-05 13:33:46 -0700

    restore project
    this removes a lot of excess code
    and updates the react app to a more recent version
    there are still a few bugs in here, but it's usable and can be worked on
    by anyone.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import AppRouter from "./routes/AppRouter";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import AppRouter from './routes/AppRouter';
+import './App.css';
 import 'tachyons';
 
 function App() {
@@ -8,56 +8,56 @@ function App() {
   const [countryList, setCountryList] = useState([]);
   const [flagList, setFlagList] = useState([]);
   const [countries, setCountries] = useState([]);
-  const [searchField, setSearchField] = useState("");
+  const [searchField, setSearchField] = useState('');
   const [userLocation, setUserLocation] = useState([]);
 
   useEffect(() => {
-    fetch("https://restcountries.eu/rest/v2/all")
-      .then(response => response.json())
-      .then(countries => {
+    fetch('https://restcountries.com/v2/all')
+      .then((response) => response.json())
+      .then((countries) => {
         let regionList = [];
         let countryList = [];
         let flagList = [];
 
         countries.map((list, i) => {
-          const { region, name, flag } = list
+          const { region, name, flag } = list;
           return (
-            regionList.push(region),
-            countryList.push(name),
-            flagList.push(flag)
+            regionList.push(region), countryList.push(name), flagList.push(flag)
           );
-        }); 
+        });
+
         setCountries(countries);
         setRegionList(findUniqRegions(regionList));
         setCountryList(countryList);
         setFlagList(flagList);
-        navigator.geolocation.getCurrentPosition(function(position) {
+        navigator.geolocation.getCurrentPosition(function (position) {
           setUserLocation(position.coords);
         });
-      });
+      })
+      .catch((error) => console.log(error));
   }, []);
 
- const findUniqRegions = (regionList) => { 
-     return Array.from(new Set(regionList.filter(region => region !== "").sort()));
-   }
+  const findUniqRegions = (regionList) => {
+    return Array.from(
+      new Set(regionList.filter((region) => region !== '').sort())
+    );
+  };
 
   const onSearchChange = (event) => {
     setSearchField(event.target.value.toLowerCase());
   };
 
-
-    return (
-      <AppRouter 
-        onSearchChange={onSearchChange} 
-        regionList={regionList}
-        countryList={countryList}
-        flagList={flagList}
-        countries={countries}
-        searchField={searchField}
-        userLocation={userLocation}
-      />
-    );
-
+  return (
+    <AppRouter
+      onSearchChange={onSearchChange}
+      regionList={regionList}
+      countryList={countryList}
+      flagList={flagList}
+      countries={countries}
+      searchField={searchField}
+      userLocation={userLocation}
+    />
+  );
 }
 
 export default App;
