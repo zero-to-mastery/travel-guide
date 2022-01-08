520b50d8de07d07dc9cda9b2e513d201453427f7
Ej1seven <erik.jamison.hunter@gmail.com>
2022-01-08 12:00:11 -0600

    added my name to the contributors list

--- src/App.js
+++ src/App.js
@@ -1,57 +1,63 @@
-import React, { Component } from "react";
-import AppRouter from "./routes/AppRouter";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import AppRouter from './routes/AppRouter';
+import './App.css';
 import 'tachyons';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      regionList: [],
-      countryList: [],
-      flagList: [],
-      countries: [],
-      searchField: ""
-    };
-  }
+function App() {
+  const [regionList, setRegionList] = useState([]);
+  const [countryList, setCountryList] = useState([]);
+  const [flagList, setFlagList] = useState([]);
+  const [countries, setCountries] = useState([]);
+  const [searchField, setSearchField] = useState('');
+  const [userLocation, setUserLocation] = useState([]);
 
-  componentDidMount() {
-    fetch("https://restcountries.eu/rest/v2/all")
-      .then(response => response.json())
-      .then(countries => {
+  useEffect(() => {
+    fetch('https://restcountries.com/v2/all')
+      .then((response) => response.json())
+      .then((countries) => {
         let regionList = [];
         let countryList = [];
         let flagList = [];
 
         countries.map((list, i) => {
-          const { region, name, flag } = list
+          const { region, name, flag } = list;
           return (
-            regionList.push(region),
-            countryList.push(name),
-            flagList.push(flag)
+            regionList.push(region), countryList.push(name), flagList.push(flag)
           );
-        }); 
-        this.setState({ countries: countries });
-        this.setState({ regionList: this.findUniqRegions(regionList) });
-        this.setState({ countryList });
-        this.setState({ flagList });
-      });
+        });
 
-  }
+        setCountries(countries);
+        setRegionList(findUniqRegions(regionList));
+        setCountryList(countryList);
+        setFlagList(flagList);
+        navigator.geolocation.getCurrentPosition(function (position) {
+          setUserLocation(position.coords);
+        });
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
- findUniqRegions = (regionList) => { 
-     return Array.from(new Set(regionList.filter(region => region !== "").sort()));
-   }
-
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value.toLowerCase() });
+  const findUniqRegions = (regionList) => {
+    return Array.from(
+      new Set(regionList.filter((region) => region !== '').sort())
+    );
   };
 
-  render() {
-    return (
-      <AppRouter onSearchChange={this.onSearchChange} state={this.state} />
-    );
-  }
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value.toLowerCase());
+  };
+
+  return (
+    <AppRouter
+      onSearchChange={onSearchChange}
+      regionList={regionList}
+      countryList={countryList}
+      flagList={flagList}
+      countries={countries}
+      searchField={searchField}
+      userLocation={userLocation}
+    />
+  );
 }
 
 export default App;
--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -4,13 +4,26 @@ import "../styles/CountryCard.css";
 const CountryCard = props => {
   const bcg = {
     backgroundImage: `url(${props.countryData.flag})`,
-    backgroundSize: "200px auto",
+    backgroundSize: "184px auto",
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center",
-    // backgroundColor: "white",
-    cursor: "pointer"
+    cursor: "pointer",
+    color: "black"
   };
-  return <div style={bcg} className="card">{props.countryData.name}</div>
+  const stl = {
+    cursor: "pointer",
+    color: "black",
+    fontSize: 15,
+    margin: 0
+  };
+  return (
+    <div className="card">
+      <div className="card__title">
+        <h1 style={stl}>{props.countryData.name}</h1>
+      </div>
+      <div style={bcg} className="card__image"></div>
+    </div>
+  );
 };
 
 export default CountryCard;
