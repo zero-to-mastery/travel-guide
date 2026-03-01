2109093e9d988aa01989d3561b5d20c30ed89c39
Laura White <lmwhite7676@gmail.com>
2026-03-01 14:42:21 -0600

    upgrades to React 18 plus related dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'tachyons';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
